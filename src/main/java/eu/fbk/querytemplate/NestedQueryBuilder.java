4c4029d72fe346178397bd269ec8dd5c3e55da80
Michele Mostarda <michele.mostarda@gmail.com>
2015-01-07 17:59:00 +0100

    Added nested query builder from JSON config.

---
+++ src/main/java/eu/fbk/querytemplate/NestedQueryBuilder.java
@@ -0,0 +1,72 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package eu.fbk.querytemplate;
+
+import org.codehaus.jackson.JsonNode;
+import org.codehaus.jackson.JsonParser;
+import org.codehaus.jackson.map.ObjectMapper;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Builds a {@link eu.fbk.querytemplate.NestedQuery} from a JSON config file.
+ *
+ * @author Michele Mostarda (mostarda@fbk.eu)
+ */
+public class NestedQueryBuilder {
+
+    NestedQuery build(JsonNode root) {
+        final JsonNode queries = root.get("queries");
+        if(!queries.isArray()) throw new IllegalArgumentException("queries field must be aan array");
+        final DefaultNestedQuery defaultNestedQuery = new DefaultNestedQuery();
+        for(JsonNode queryNode : queries) {
+            defaultNestedQuery.addQuery(getName(queryNode), processQuery(queryNode));
+        }
+        return defaultNestedQuery;
+    }
+
+    NestedQuery build(InputStream is) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        final JsonParser parser = mapper.getJsonFactory().createJsonParser(is);
+        return build(parser.readValueAsTree());
+    }
+
+    private String getName(JsonNode queryNode) {
+        return queryNode.get("name").asText();
+    }
+
+    private Query processQuery(JsonNode queryNode) {
+        final String template = queryNode.get("query").asText();
+        final String[] inVars = toStringArray(queryNode.get("in_vars"));
+        final String[] outVars = toStringArray(queryNode.get("out_vars"));
+        return new DefaultQuery(template, inVars, outVars);
+    }
+
+    private String[] toStringArray(JsonNode list) {
+        if(!list.isArray()) throw new IllegalArgumentException();
+        final String[] out = new String[list.size()];
+        int i = 0;
+        for(JsonNode e : list) {
+            out[i++] = e.asText();
+        }
+        return out;
+    }
+
+
+}
